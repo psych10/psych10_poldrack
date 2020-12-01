0123cace401077521f3cbf6014338f83576b12f6
poldrack <poldrack@gmail.com>
2020-12-01 07:30:37 -0800

    fix parens

--- problem_sets/2020/tests/week2_tests.R
+++ problem_sets/2020/tests/week2_tests.R
@@ -4,7 +4,7 @@ check_values <- function(problem){
   if (problem == 1){
     assert_that(is.numeric(my_age))
     assert_that(is.character(my_major))
-    assert_that(is.logical(i_am_senior)
+    assert_that(is.logical(i_am_senior))
   }
   print('good job!')
 }
