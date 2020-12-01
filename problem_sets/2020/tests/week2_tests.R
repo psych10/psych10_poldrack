1a3c1d27517fb7c14b1bc81e46173e1b5fbb7673
poldrack <poldrack@gmail.com>
2020-12-01 07:24:50 -0800

    initial add

---
+++ problem_sets/2020/tests/week2_tests.R
@@ -0,0 +1,8 @@
+library(assertthat)
+
+check_values <- function(){
+  assert_that(is.numeric(my_age))
+  assert_that(is.character(my_major))
+  assert_that(is.logical(i_am_senior))
+  print('good job!')
+}
