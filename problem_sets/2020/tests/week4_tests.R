21d648d9bdbaf742ff645a3be1ccabe610c7500b
poldrack <poldrack@gmail.com>
2021-01-14 15:30:52 -0800

    initial add

---
+++ problem_sets/2020/tests/week4_tests.R
@@ -0,0 +1,12 @@
+library(assertthat)
+
+check_values <- function(problem){
+  if (problem == 1){
+  } else if (problem == 2){
+  } else if (problem == 3){
+   } else if (problem == 4){
+  } else if (problem == 5){
+  } else if (problem == 6){
+  }
+  print('good job!')
+}
