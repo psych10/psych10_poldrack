a0cc89dbf8fb28806fb446567b8d970a3322f09d
Russ Poldrack <poldrack@gmail.com>
2023-02-02 15:16:15 -0800

    update for new pset

--- problem_sets/2023/tests/week8_tests.R
+++ problem_sets/2023/tests/week8_tests.R
@@ -1,73 +1,87 @@
 library(assertthat)
 
 check_values <- function(problem){
+  NHANES_a <- drop_na(filter(NHANES, Age >= 30, Age <= 40), AlcoholYear, HealthGen)
+  
+  # create the drinks_regularly and good_health variables
+  NHANES_a <- mutate(NHANES_a,
+                     drinks_regularly = AlcoholYear > 30,
+                     good_health = HealthGen %in% c('Excellent', 'Vgood', 'Good'))
+  cr <- chisq.test(NHANES_a$drinks_regularly, NHANES_a$good_health)
+  o_goodhealth_drinks <- cr$observed[2, 2] / cr$observed[2, 1]
+  o_goodhealth_nodrinks <- cr$observed[1, 2] / cr$observed[1, 1]
+  o_ratio <- o_goodhealth_drinks / o_goodhealth_nodrinks
+  
+  NHANES_b <- NHANES %>% filter(Age > 17, Age < 60) %>% drop_na(TotChol)
+  cor_a_c <- cor(NHANES_b$Age, NHANES_b$TotChol)
+  pwr_res <- pwr.r.test(r=cor_a_c, power=.8, sig.level=.05)
+  samp_size <- round(pwr_res$n)
+  n_samps = 1000
+  result_df_test <- data.frame(pvalue=rep(NA, n_samps),
+             corr = rep(NA, n_samps)) 
+
   if (problem == 1){
-    assert_that(chisq_result$statistic
-                == chisq.test(maskdata$WearingMask, maskdata$TouchingFace)$statistic)
-    
+    assert_that(all_equal(NHANES_adult, NHANES_a))
+
   } else if (problem == 2){
-    assert_that(all_equal(std_residuals, chisq_result$residuals))
+    assert_that(cr$statistic == chisq_result$statistic)
     
   }else if (problem == 3){
-    assert_that(odds_touching_mask == chisq_result$observed[2, 2] / chisq_result$observed[2, 1])
-    assert_that(odds_touching_nomask == chisq_result$observed[1, 2] / chisq_result$observed[1, 1])
-    assert_that(odds_ratio == odds_touching_nomask / odds_touching_mask)
+    assert_that(all_equal(std_residuals, chisq_result$residuals))
     
   } else if (problem == 4){
-    assert_that(power_result$n == power.t.test(delta=0.5, power=0.8)$n)
+    assert_that(o_goodhealth_drinks == odds_goodhealth_drinks)
+    assert_that(o_goodhealth_nodrinks == odds_goodhealth_nodrinks)
+    assert_that(odds_ratio == o_ratio)
+
   } else if (problem == 5){
-    assert_that(n_simulations == 1000)
-    
-    assert_that(sample_size_per_group == round(power_result$n))
-    
-    assert_that(mean_wings == 100)
-    assert_that(sd_wings == 10)
-    
-    assert_that(all_equal(result_df, data.frame(pvalue=rep(NA, n_simulations),
-                                                cohensd = rep(NA, n_simulations))))
+    assert_that(cor_a_c == cor_age_chol)
+    assert_that(power_result$n == pwr_res$n)
     
   } else if (problem == 6){
+    assert_that(n_samples == n_samps)
     
-    result_df_test <- data.frame(pvalue=rep(NA, n_simulations),
-                                 cohensd = rep(NA, n_simulations))
-    set.seed(1) 
-    cohensd_test <- 0.2
-    assert_that(cohensd == cohensd_test)
+    assert_that(sample_size == samp_size)
     
-    for (i in 1:n_simulations){
-      sport_test <- c(rep('football', sample_size_per_group), 
-                      rep('basketball', sample_size_per_group))
+    assert_that(all_equal(result_df, result_df_test))
+    
+    
+  } else if (problem == 7){
+    set.seed(1234)  # DO NOT CHANGE THIS - it ensures the same result each time
+    
+    # Now loop through for all of the n_samples runs
+    for (i in 1:n_samps){
       
-      wings_test <- rnorm(sample_size_per_group*2, mean=mean_wings, sd=sd_wings)
+      # take a sample from the dataset, using the sample size determined via power analysis
+      sample_df <- sample_n(NHANES_adult, samp_size)
       
-      group_difference_test <- cohensd_test * sd(wings_test)
+      # perform a correlation test to compare wing consumption between the groups
+      corr_result <- cor.test(sample_df$Age, sample_df$TotChol)
       
-      wings_test = wings_test + group_difference_test * (sport_test == 'basketball')
+      # store the pvalue from the correlation test into our data frame
+      result_df_test$pvalue[i] <- corr_result$p.value
       
-      sim_df_test <- data.frame(sport=sport_test, wings=wings_test)
-      
-      ttest_result_test <- t.test(wings ~ sport, sim_df_test)
-      
-      result_df_test$pvalue[i] = ttest_result_test$p.value
-      
-      cohensd_result_test <- cohen.d(wings ~ sport, sim_df_test)
-      
-      result_df_test$cohensd[i] = cohensd_result_test$estimate
+      # the correlation estimate is stored in the estimate variable within
+      # the output from the cor.test function.  extract that value and save 
+      # it to our data frame for this simulation
+      result_df_test$corr[i] <- corr_result$estimate
       
     }
     assert_that(all_equal(result_df, result_df_test))
-  } else if (problem == 7){
-    assert_that(power == mean(result_df$pvalue < .05))
     
   } else if (problem == 8){
-    assert_that(mean_cohensd == mean(result_df$cohensd))
+    assert_that(power == mean(result_df$pvalue < .05))
+    
     
   } else if (problem == 9){
-    cohensd_summary_test <- result_df %>%
+    assert_that(mean_corr == mean(result_df$corr))
+  } else if (problem == 10){
+    corr_summary_test <- result_df %>%
       mutate(significant = pvalue < .05) %>%
       group_by(significant) %>%
-      summarize(mean_cohensd = mean(cohensd))
-    assert_that(all_equal(cohensd_summary, cohensd_summary_test))
+      summarize(mean_corr = mean(corr))
+    
+    assert_that(all_equal(corr_summary, corr_summary_test))
   }
   print('good job!')
 }
